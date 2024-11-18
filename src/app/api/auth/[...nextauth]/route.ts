47b762723c6f0e1ff94eed99a6ecd7796dfd545f
cristian guedes <cristianguedes136@gmail.com>
2024-11-18 08:52:03 -0300

    feat:with-prisma

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,18 @@
 import GoogleProvider from "next-auth/providers/google";
 import NextAuth, { AuthOptions } from "next-auth";
+import Adapters from "next-auth/adapters";
+
+import { PrismaClient } from "@prisma/client";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 //For production: https://{YOUR_DOMAIN}/api/auth/callback/google
 //For development: http://localhost:3000/api/auth/callback/google
 // se the availables provider in /api/auth/providers
 
+export const prisma = new PrismaClient();
+
 export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
@@ -13,6 +20,9 @@ export const authOptions: AuthOptions = {
     }),
   ],
   debug: true,
+  session: {
+    strategy: "database",
+  },
 };
 const handler = NextAuth(authOptions);
 
--- src/app/restrict-sample/check-session-server-side/page.tsx
+++ src/app/restrict-sample/check-session-server-side/page.tsx
@@ -1,9 +1,10 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { LogoutButton } from "@/components/logoutButton";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
 export default async function Page() {
-  const session = await getServerSession();
+  const session = await getServerSession(authOptions);
   if (!session) {
     redirect("/");
   }
